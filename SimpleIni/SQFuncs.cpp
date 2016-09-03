d752589a0938d5895e99f377b8557c766508306d
Ankris <merkel.naller@outlook.es>
2016-09-03 03:27:57 +0200

    Update 2
    Some fixes, code cleanup and param checker.

--- SimpleIni/SQFuncs.cpp
+++ SimpleIni/SQFuncs.cpp
@@ -8,6 +8,10 @@ extern HSQAPI sq;
 extern HSQUIRRELVM v;
 
 _SQUIRRELDEF(INI_Open) {
+	if (sq->gettype(v, 2) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid filename."));
+	}
+
 	CSimpleIni *ini = new CSimpleIni();
 
 	const SQChar *file;
@@ -21,6 +25,14 @@ _SQUIRRELDEF(INI_Open) {
 }
 
 _SQUIRRELDEF(INI_SaveFile) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid filename."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -36,6 +48,10 @@ _SQUIRRELDEF(INI_SaveFile) {
 }
 
 _SQUIRRELDEF(INI_Close) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -50,6 +66,10 @@ _SQUIRRELDEF(INI_Close) {
 }
 
 _SQUIRRELDEF(INI_Exists) {
+	if (sq->gettype(v, 2) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid filename."));
+	}
+
 	const SQChar *name;
 	sq->getstring(v, 2, &name);
 
@@ -67,6 +87,10 @@ _SQUIRRELDEF(INI_Exists) {
 }
 
 _SQUIRRELDEF(INI_IsEmpty) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -79,6 +103,14 @@ _SQUIRRELDEF(INI_IsEmpty) {
 }
 
 _SQUIRRELDEF(INI_SetUnicode) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_BOOL) {
+		return sq->throwerror(v, _SC("Error: Invalid bool: not true nor false."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -94,6 +126,14 @@ _SQUIRRELDEF(INI_SetUnicode) {
 }
 
 _SQUIRRELDEF(INI_GetSectionSize) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -109,6 +149,18 @@ _SQUIRRELDEF(INI_GetSectionSize) {
 }
 
 _SQUIRRELDEF(INI_DeleteSection) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -121,9 +173,6 @@ _SQUIRRELDEF(INI_DeleteSection) {
 	const SQChar *key;
 	sq->getstring(v, 4, &key);
 
-	const SQChar *value;
-	sq->getstring(v, 5, &value);
-
 	ini->Delete(section, key, true);
 
 	sq->pushbool(v, SQTrue);
@@ -132,6 +181,22 @@ _SQUIRRELDEF(INI_DeleteSection) {
 }
 
 _SQUIRRELDEF(INI_SetString) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
+	if (sq->gettype(v, 5) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid value, not a string."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -155,6 +220,22 @@ _SQUIRRELDEF(INI_SetString) {
 }
 
 _SQUIRRELDEF(INI_SetBool) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
+	if (sq->gettype(v, 5) != OT_BOOL) {
+		return sq->throwerror(v, _SC("Error: Invalid value, not a bool."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -178,6 +259,22 @@ _SQUIRRELDEF(INI_SetBool) {
 }
 
 _SQUIRRELDEF(INI_SetFloat) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
+	if (sq->gettype(v, 5) != OT_FLOAT) {
+		return sq->throwerror(v, _SC("Error: Invalid value, not a float."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -201,6 +298,22 @@ _SQUIRRELDEF(INI_SetFloat) {
 }
 
 _SQUIRRELDEF(INI_SetInteger) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
+	if (sq->gettype(v, 5) != OT_INTEGER) {
+		return sq->throwerror(v, _SC("Error: Invalid value, not an integer."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -223,7 +336,19 @@ _SQUIRRELDEF(INI_SetInteger) {
 	return 1;
 }
 
-_SQUIRRELDEF(INI_ReadString) {
+_SQUIRRELDEF(INI_GetString) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -237,12 +362,30 @@ _SQUIRRELDEF(INI_ReadString) {
 	sq->getstring(v, 4, &key);
 
 	const char *str = ini->GetValue(section, key);
+	if (str == 0 || sizeof str == 0 || strlen(str) == 0) {
+		sq->pushnull(v);
+
+		return 1;
+	}
+
 	sq->pushstring(v, str, strlen(str));
 
 	return 1;
 }
 
-_SQUIRRELDEF(INI_ReadBool) {
+_SQUIRRELDEF(INI_GetBool) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -261,7 +404,19 @@ _SQUIRRELDEF(INI_ReadBool) {
 	return 1;
 }
 
-_SQUIRRELDEF(INI_ReadFloat) {
+_SQUIRRELDEF(INI_GetFloat) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -280,7 +435,19 @@ _SQUIRRELDEF(INI_ReadFloat) {
 	return 1;
 }
 
-_SQUIRRELDEF(INI_ReadInteger) {
+_SQUIRRELDEF(INI_GetInteger) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
+	if (sq->gettype(v, 3) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid section, not a string."));
+	}
+
+	if (sq->gettype(v, 4) != OT_STRING) {
+		return sq->throwerror(v, _SC("Error: Invalid key, not a string."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
@@ -293,24 +460,22 @@ _SQUIRRELDEF(INI_ReadInteger) {
 	const SQChar *key;
 	sq->getstring(v, 4, &key);
 
-	long val = ini->GetLongValue(section, key);
+	SQInteger val = ini->GetLongValue(section, key);
 	sq->pushinteger(v, val);
 
 	return 1;
 }
 
 _SQUIRRELDEF(INI_Reset) {
+	if (sq->gettype(v, 2) != OT_USERPOINTER) {
+		return sq->throwerror(v, _SC("Error: Invalid INI pointer."));
+	}
+
 	SQUserPointer ptr = NULL;
 	sq->getuserpointer(v, 2, &ptr);
 
 	CSimpleIni *ini = NULL;
 	ini = (CSimpleIni*)ptr;
-
-	const SQChar *section;
-	sq->getstring(v, 3, &section);
-
-	const SQChar *key;
-	sq->getstring(v, 4, &key);
 
 	ini->Reset();
 
@@ -353,12 +518,12 @@ void RegisterFuncs(HSQUIRRELVM v) {
 	RegisterSquirrelFunc(v, INI_SetString, "INI_SetString", 0, 0);
 	RegisterSquirrelFunc(v, INI_SetBool, "INI_SetBool", 0, 0);
 	RegisterSquirrelFunc(v, INI_SetFloat, "INI_SetFloat", 0, 0);
-	RegisterSquirrelFunc(v, INI_SetString, "INI_SetInteger", 0, 0);
+	RegisterSquirrelFunc(v, INI_SetInteger, "INI_SetInteger", 0, 0);
 
-	RegisterSquirrelFunc(v, INI_ReadString, "INI_ReadString", 0, 0);
-	RegisterSquirrelFunc(v, INI_ReadBool, "INI_ReadBool", 0, 0);
-	RegisterSquirrelFunc(v, INI_ReadFloat, "INI_ReadFloat", 0, 0);
-	RegisterSquirrelFunc(v, INI_ReadInteger, "INI_ReadInteger", 0, 0);
+	RegisterSquirrelFunc(v, INI_GetString, "INI_GetString", 0, 0);
+	RegisterSquirrelFunc(v, INI_GetBool, "INI_GetBool", 0, 0);
+	RegisterSquirrelFunc(v, INI_GetFloat, "INI_GetFloat", 0, 0);
+	RegisterSquirrelFunc(v, INI_GetInteger, "INI_GetInteger", 0, 0);
 
 	RegisterSquirrelFunc(v, INI_Reset, "INI_Reset", 0, 0);
 }
